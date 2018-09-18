626e463fcc604c052aea227af10e20c14c8a6db9
Pouya Larjani <plj@fb.com>
2018-09-17 23:40:45 -0700

    Entity object and load/save

---
+++ src/model/Entity.ts
@@ -0,0 +1,79 @@
+export default class Entity {
+  public name: string;
+
+  private _parent?: Entity;
+  public readonly children = new Set<Entity>();
+
+  constructor(
+    // Unique identifier for this entity
+    public readonly id = Math.random().toString(36).substr(2, 9),
+    // Last time vectors were calculated, [s]
+    protected lastUpdated: number = Date.now() / 1000,
+    // Type of this entity
+    public readonly type = 'Entity',
+  ) {
+    this.name = id;
+  }
+
+  get parent() {
+    return this._parent;
+  }
+
+  set parent(parent: Entity | undefined) {
+    if (this._parent) {
+      this._parent.children.delete(this);
+    }
+
+    if (parent && (parent != this)) {
+      this._parent = parent;
+    } else {
+      this._parent = undefined;
+    }
+
+    if (this._parent) {
+      this._parent.children.add(this);
+    }
+  }
+
+  save(): any {
+    let res: any = {};
+
+    res.id = this.id;
+    res.updated = this.lastUpdated;
+
+    res.name = this.name;
+
+    res.children = Array.from(this.children).map(child => child.save())
+    if (this.parent) {
+      res.parent = this.parent.id;
+    }
+
+    return res;
+  }
+
+  load(_: any): Entity {
+    return this;
+  }
+
+  static loadNew(data: any): Entity {
+    let entity = new Entity(
+      data.id || undefined,
+      data.updated || undefined,
+      data.type || undefined,
+    );
+
+    switch (data.type || 'Entity') {
+      case 'Entity':
+      default:
+        break;
+    }
+
+    entity.name = data.name || entity.name;
+
+    for (let child of data.children || []) {
+      entity.children.add(Entity.loadNew(child));
+    }
+
+    return entity.load(data);
+  }
+}
